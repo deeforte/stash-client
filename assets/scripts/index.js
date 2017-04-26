128784dfc3224b3d4a11b533e1c2f1c342713d1d
deeforte <deirdre.forte@libertymutual.com>
2017-04-26 18:53:54 -0400

    Commit for stash client first prototype deliverable
    commit for the first step in outlining stash client

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,6 +2,55 @@
 
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
+console.log('up and running')
+const yarn = [
+{
+	name: 'mountain mohair',
+  quantity: 7,
+  length: 200,
+	fiber: 'wool-mohair',
+  color: 'gray',
+  weight: 'worsted',
+  project: 'sweater'
+},
+{
+	name: 'tidal yarns',
+  quantity: 4,
+  length: 400,
+	fiber: 'alpaca',
+  color: 'blue',
+  weight: 'dk',
+  project: 'sweater'
+},
+{
+	name: 'longmeadow farms',
+  quantity: 1,
+  length: 800,
+	fiber: 'bfl',
+  color: 'mustard',
+  weight: 'dk',
+  project: 'shawl'
+},
+{
+	name: 'harrisville highland',
+  quantity: 3,
+  length: 200,
+	fiber: 'wool',
+  color: 'off white',
+  weight: 'worsted',
+  project: 'scarf'
+},
+]
+const listYarns = function(array) {
+	  for (let i=0; i<array.length; i++) {
+			console.log('inlistYarns')
+      var ul = document.getElementById("yarnList")
+      var li = document.createElement("li")
+      li.appendChild(document.createTextNode(' - ' + yarn[i].name + ' ' + yarn[i].quantity + '- ' + yarn[i].length + ' yd skeins ' + yarn[i].fiber + ' in ' + yarn[i].color + ' ' + yarn[i].weight + ' weight planned for ' + yarn[i].project))
+      ul.appendChild(li);
+	   }
+}
+listYarns(yarn)
 
 $(() => {
   setAPIOrigin(location, config)
