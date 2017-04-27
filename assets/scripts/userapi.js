eb84661046260fb30376b88e961ac6a3016b5e7b
deeforte <deirdre.forte@libertymutual.com>
2017-04-27 11:55:58 -0400

    Commit for stash project
    commit includes getting basic layout, singup and signin.   Have not
    tested on heroku.

---
+++ assets/scripts/userapi.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const config = require('./config')
+const store = require('./store')
+
+const signUp = (data) => {
+  return $.ajax({
+    url: config.apiOrigin + '/sign-up',
+    method: 'POST',
+    data
+  })
+}
+
+const signIn = (data) => {
+  return $.ajax({
+    url: config.apiOrigin + '/sign-in',
+    method: 'POST',
+    data
+  })
+}
+
+module.exports = {
+  signUp,
+  signIn,
+//  signOut,
+//  chgPswd
+}
---
+++ assets/scripts/userui.js
@@ -0,0 +1,45 @@
+'use strict'
+require('./index')
+
+const store = require('./store')
+//const gameEvents = require('./games.js')
+
+const signUpSuccess = (data) => {
+  console.log('user signed up -', data)
+//  document.getElementById('sign-up').reset
+//  $('#sign-up').hide()
+}
+const signUpFailure = () => {
+  console.error('signup error')
+}
+const signInSuccess = (data) => {
+  console.log('signin success ran data is ', data)
+  store.user = data.user
+//  $('h2').text('')
+//  $('h4').text('Scoreboard: no games played since signin')
+//  $('h5').text(store.user.email)
+//  $('.signinForms').hide()
+//  $('.signoutForms').css('visibility', 'visible')
+//  $('.gameBoard').css('visibility', 'visible')
+  // gameEvents.getUserStats()
+  // console.log(store.games)
+  // console.log(store)
+//  gameEvents.getUserStats()
+//  gameEvents.gameCreate()
+//  console.log(store)
+}
+
+const signInFailure = () => {
+//  console.error(error)
+}
+
+module.exports = {
+  signUpSuccess,
+  signUpFailure,
+  signInSuccess,
+  signInFailure,
+//  signOutSuccess,
+//  signOutFailure,
+//  chgPswdSuccess,
+//  chgPswdFailure
+}
